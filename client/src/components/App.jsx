5f2fd41e563a04dd8941a22feede813eb2c132bc
Jonathan Kwak <jkwak09@gmail.com>
2020-02-28 16:07:40 -0500

    Setup intial root component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render{
+    return(
+      <>
+
+      </>
+    )
+  }
+};
+
+export default App;
